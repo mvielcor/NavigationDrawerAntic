861ebbaf17a0d4d702414c1cafc7a2a43d9c2c55
mvielcor <mvielcor@gmail.com>
2015-08-01 03:25:43 +0200

    Navigation Drawer amb Gestió d'Events

--- app/src/main/java/pmdm/mviel/navigationdrawerantic/MainActivity.java
+++ app/src/main/java/pmdm/mviel/navigationdrawerantic/MainActivity.java
@@ -3,11 +3,18 @@ package pmdm.mviel.navigationdrawerantic;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
 
 public class MainActivity extends Activity {
 
@@ -24,6 +31,7 @@ public class MainActivity extends Activity {
     DrawerLayout mDrawerLayout;
     GridView mGridViewDrawer;
     ArrayList<DrawerItem> LlistaItems= new ArrayList<DrawerItem>();
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +43,63 @@ public class MainActivity extends Activity {
         mGridViewDrawer = (GridView) findViewById(R.id.grid_Opcions_Nav_Drawer);
 
         // Creem un arrayList d'objectes DrawerItem
-        for(int i=0;i<mTEXTOS_OPCIONS.length;i++){
-            LlistaItems.add(new DrawerItem(mTEXTOS_OPCIONS[i],mICONS[i]));
+        for (int i = 0; i < mTEXTOS_OPCIONS.length; i++) {
+            LlistaItems.add(new DrawerItem(mTEXTOS_OPCIONS[i], mICONS[i]));
         }
         // Establim l'Adaptador del GridView
-        mGridViewDrawer.setAdapter(new DrawerGridAdapter(this,LlistaItems));
+        mGridViewDrawer.setAdapter(new DrawerGridAdapter(this, LlistaItems));
 
-/*        mGridViewDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // Afegim un Listener per a que reaccione a la nostra selecció
+        mGridViewDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Toast.makeText(getApplicationContext(), "" + position, Toast.LENGTH_SHORT).show();
+                // Accedim al Linear Layout definit en item_gridview_drawerlayout.xml
+                LinearLayout ll = (LinearLayout) v;
+                //accedim a l'Element TextView que ocupa la segona posició
+                TextView tv = (TextView) ll.getChildAt(1);
+                //Mostrem un Toast amb el text i la posició que ocupa l'ítem selecionat dins del gridview
+                Toast.makeText(getApplicationContext(), tv.getText() + " (" + position + ")", Toast.LENGTH_SHORT).show();
             }
         });
-    */
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Mostrem la icona de navegació a l'ActionBar
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        // Activem l'icona de navegació de l'ActionBar per a que puga mostrar i ocultar el navigationDrawer
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // Ací posarem el codi que vulguem que s'execute quan el navigationDrawer esta obert
+                //Per fer una prova, li direm que mostre el text "Obrint Drawer"
+                getActionBar().setTitle(R.string.openDrawer);
+                //Sincronitzem (important)
+                mDrawerToggle.syncState();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Ací posarem el codi que vulguem que s'execute quan el navigationDrawer esta obert
+                //Per fer una prova, li direm que mostre el text "Tancant Drawer"
+                getActionBar().setTitle(R.string.closeDrawer);
+                //Sincronitzem (important)
+                mDrawerToggle.syncState();
+            }
+        };
+
+        // Li asociem com a Listener, el lístener del ActionBar
+        mDrawerLayout.setDrawerListener(mDrawerToggle); // Listener que escolta quan s'obri o tanca
+        // el NavigationDrawer des del toggle de l'ActionBar
+        mDrawerToggle.syncState();               // Finalment, sincronitzem l'estat del toggle.
     }
 
     @Override
@@ -59,16 +111,20 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // Gestionem els Clicks de l'actionBar ací. L'Action bar gestionarà els clicks de la icona
+        // Home/Up tal i com s'ha especificat al fitxer AndroidManifest.xml
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        //Si s'ha polsat L'icona de l'actionBar, tornarem true, per indicar que hem gestionat l'event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        //Si s'ha polsat el text de configuratió, tornarem true per indicar que hem gestionat l'event
         if (id == R.id.action_settings) {
             return true;
         }
-
+        //En qualsevol altre cas, tornarem super.onOptionsItemSelected(item) per passar l'event a
+        // la classe pare, i que el gestione ella.
         return super.onOptionsItemSelected(item);
     }
 
